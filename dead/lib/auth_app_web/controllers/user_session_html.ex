04089dff50445b7b776522b8ea4a3673a2667f64
Steffen Deusch <steffen@deusch.me>
2025-02-10 11:21:19 +0100

    add local mail info box

--- dead/lib/auth_app_web/controllers/user_session_html.ex
+++ dead/lib/auth_app_web/controllers/user_session_html.ex
@@ -2,4 +2,8 @@ defmodule AuthAppWeb.UserSessionHTML do
   use AuthAppWeb, :html
 
   embed_templates "user_session_html/*"
+
+  defp local_mail_adapter? do
+    Application.get_env(:auth_app, AuthApp.Mailer)[:adapter] == Swoosh.Adapters.Local
+  end
 end
